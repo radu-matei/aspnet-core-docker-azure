3d1795419b266b2ae8454c2a95f36a7e41a8c68f
Radu Matei <matei.radu94@gmail.com>
2016-11-27 02:24:13 +0200

    Added basic web application

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .Configure(app => app.Run(context =>
+                    {
+                      return context.Response.WriteAsync($"Hello, Universe! It is {DateTime.Now}");
+                    }))
+                .Build();
+
+            host.Run();
+        }
+    }
+}
