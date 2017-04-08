c82a8ac3ff5f6f549ed6f08361c51e7f23b7f83a
Radu Matei <matei.radu94@gmail.com>
2017-04-08 09:12:19 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleApplication
                 .UseKestrel()
                 .Configure(app => app.Run(context =>
                     {
-                      return context.Response.WriteAsync($"Hello, CodeCamp!! It is {DateTime.Now}");
+                      return context.Response.WriteAsync($"This is really cool!!! It is {DateTime.Now}");
                     }))
                 .Build();
 
